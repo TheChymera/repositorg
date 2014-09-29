0725002202fd4be2536d311701fc1d2524ade3b6
Horea Christian <h.chr@mail.ru>
2014-09-29 21:33:11 +0200

    addded cli function

---
+++ organamer_reposit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+__author__ = 'Horea Christian'
+import argparse
+import organamer as on
+
+parser = argparse.ArgumentParser()
+parser.add_argument("destination", help="Path to store files into (excluding alphanumeric storage directories)", type=str)
+parser.add_argument("source", help="Path to reposit files from (all subdirectories will be crawled!)", type=str)
+parser.add_argument("-q", "--quiet", help="Do not ask for confirmation - DANGEROUS!", action="store_false")
+args = parser.parse_args()
+
+on.reposit(destination_root=args.destination, source_root=args.source, prompt=args.quiet)
