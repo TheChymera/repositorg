40c56b3304b97238410d0eac86fe7c741846689e
Horea Christian <h.chr@mail.ru>
2014-09-29 08:25:36 +0200

    added setup file for PIP easy install

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name="organamer",
+    version="",
+    description = "Automatically organize (and rename) large collections of files",
+    author = "Horea Christian",
+    author_email = "h.chr@mail.ru",
+    url = "https://github.com/TheChymera/organamer",
+    keywords = ["rename", "organize", "batch"],
+    py_modules = ["organamer", "organamer_reposit"],
+    classifiers = [],
+    install_requires=[],
+    )
