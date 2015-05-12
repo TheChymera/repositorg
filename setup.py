47dc5e397369db0ff07944bcde82dad9f685011d
Horea Christian <h.chr@mail.ru>
2015-05-13 00:33:23 +0200

    tentatively implemented entry points

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
+
 setup(
-    name="organamer",
-    version="",
-    description = "Automatically organize (and rename) large collections of files",
-    author = "Horea Christian",
-    author_email = "h.chr@mail.ru",
-    url = "https://github.com/TheChymera/organamer",
-    keywords = ["rename", "organize", "batch"],
-    py_modules = ["organamer", "organamer_reposit"],
-    classifiers = [],
-    install_requires=[],
+	name="organamer",
+	version="",
+	description = "Automatically organize (and rename) large collections of files",
+	author = "Horea Christian",
+	author_email = "h.chr@mail.ru",
+	url = "https://github.com/TheChymera/organamer",
+	keywords = ["rename", "organize", "batch"],
+	packages = find_packages("src"),
+	package_dir = {"":"src"},
+	classifiers = [],
+	install_requires=[],
+	entry_points = {'console_scripts' : \
+			['organamer_reposit = organamer_reposit:main']
+		}
     )
