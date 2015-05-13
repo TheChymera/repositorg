cbbd19b4cd2e47270eb3316b08b2b7d55453071f
Horea Christian <h.chr@mail.ru>
2015-05-13 17:25:52 +0200

    completed the parsing

--- src/organamer/cli.py
+++ src/organamer/cli.py
@@ -7,8 +7,8 @@ def reposit():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("destination", help="Path to store files into (excluding alphanumeric storage directories)", type=str)
 	parser.add_argument("source", help="Path to reposit files from (all subdirectories will be crawled!)", type=str)
-	parser.add_argument("-f", "--force-lookup", help="User and password for your remote file source (format: `user%password`)", type=str)
+	parser.add_argument("-u", "--user-password", help="User and password for your remote file source (format: `user%password`)", type=str)
 	parser.add_argument("-q", "--quiet", help="Do not ask for confirmation - DANGEROUS!", action="store_false")
 	args = parser.parse_args()
 
-	base.reposit(destination_root=args.destination, source_root=args.source, prompt=args.quiet)
+	base.reposit(destination_root=args.destination, source_root=args.source, prompt=args.quiet, user_password=args.user_password)
