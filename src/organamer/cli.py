0c74d82c26f59b6d4dd3931781e45b7ce199c30c
Horea Christian <h.chr@mail.ru>
2015-05-14 23:24:06 +0200

    added defaults

--- src/organamer/cli.py
+++ src/organamer/cli.py
@@ -7,12 +7,12 @@ def reposit():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("destination", help="Path to store files into (excluding alphanumeric storage directories)", type=str)
 	parser.add_argument("source", help="Path to reposit files from (all subdirectories will be crawled!)", type=str)
-	parser.add_argument("-l", "--letters", help="Prepend the specified number of letters to theincremental nummeration (default is 1).", type=int)
+	parser.add_argument("-l", "--letters", help="Prepend the specified number of letters to theincremental nummeration (default is 1).", default=1, type=int)
 	parser.add_argument("-e", "--extension", help="Filter by this extension (currently works for SAMBA share download ONLY).", type=str)
 	parser.add_argument("-p", "--prefix", help="Add this prefix to all files.", type=str)
 	parser.add_argument("-a", "--parent-prefix", help="Add the name of the rot dir as a prefix to all files (defult FALSE).", action="store_true")
 	parser.add_argument("-u", "--user-password", help="User and password for your remote file source (format: `user%%password`)", type=str)
-	parser.add_argument("-d", "--digits", help="Numerate files using the specified number of digits (default is 4).", type=int)
+	parser.add_argument("-d", "--digits", help="Numerate files using the specified number of digits.", default=4, type=int)
 	parser.add_argument("-q", "--quiet", help="Do not ask for confirmation - DANGEROUS!", action="store_false")
 	args = parser.parse_args()
 
@@ -23,7 +23,7 @@ def reformat():
 	parser.add_argument("directory", help="The directory containing the files that need reformatting.", type=str)
 	parser.add_argument("-q", "--quiet", help="Do not ask for confirmation - DANGEROUS!", action="store_false")
 	parser.add_argument("-i", "--letters-start-index", help="Numerate with letters starting at this index (default is None; specify values as integers: 0=a, 1=b, etc.).", type=int)
-	parser.add_argument("-d", "--digits", help="Numerate files using the specified number of digits (default is 4).", type=int)
+	parser.add_argument("-d", "--digits", help="Numerate files using the specified number of digits.", default=4, type=int)
 	parser.add_argument("-p", "--prefix", help="Add this prefix to all files.", type=str)
 	args = parser.parse_args()
 
