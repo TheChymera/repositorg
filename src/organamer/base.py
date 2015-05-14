b258aac66a49f017b83e2df2ecf01e31d81658c5
Horea Christian <h.chr@mail.ru>
2015-05-14 18:09:33 +0200

    implemented empty destination dir use case
    also added many menial improvementsand comments/docstirngs

--- src/organamer/base.py
+++ src/organamer/base.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 from __future__ import division
 __author__ = 'Horea Christian'
+import os
 
 def rename(root_dir, strip_string="", append_string="", prepend_string="", conditional=True, execute=False):
-	import os
 	root_dir = os.path.expanduser(root_dir)
 	old_filenames=[]
 	new_filenames=[]
@@ -27,6 +27,7 @@ def rename(root_dir, strip_string="", append_string="", prepend_string="", condi
 	if len(new_filenames) != len(old_filenames):
 		raise RuntimeError("Lists of old and new filenames are not of the same length. Unsafe to continue")
 
+	# actually perform the renaming operation in this function
 	if execute:
 		for i in range(len(new_filenames)):
 			os.rename(old_filenames[i], new_filenames[i])
@@ -35,7 +36,6 @@ def rename(root_dir, strip_string="", append_string="", prepend_string="", condi
 		return [old_filenames, new_filenames]
 
 def pair_lastfile(destination_files, source_files):
-	import os
 	lastfile_found = False
 
 	destination_files = sorted(destination_files)
@@ -58,7 +58,6 @@ def pair_lastfile(destination_files, source_files):
 	else:
 		destination_dir = os.path.basename(destination_files[0])
 		print("No pair for the last file from "+str(destination_dir)+", namely "+str(lastfile)+" was found.")
-		quit()
 
 def sha256_hashfile(file_path, blocks="all"):
 	import hashlib
@@ -78,10 +77,8 @@ def sha256_hashfile(file_path, blocks="all"):
 
 	return hasher.hexdigest()
 
-def reposit(destination_root, source_root, digits=4, letters=1, prefix=None, parent_prefix=True, prompt=True, user_password=None):
-	import os
+def reposit(destination_root, source_root, digits=4, letters=1, prefix=None, parent_prefix=True, prompt=True, user_password=None, estension=None):
 	import string
-	from shutil import copyfile
 
 	destination_root = os.path.expanduser(destination_root)
 	destination_files_list = []
@@ -93,14 +90,17 @@ def reposit(destination_root, source_root, digits=4, letters=1, prefix=None, par
 	if source_root[0:6] == "smb://":
 		if not user_password:
 			raise RuntimeError("Please specify a user and login for the SAMBA share. The proper format is username%password")
-		from subprocess import call
+		from subprocess import call, list2cmdline
 		import time
-		from os import makedirs, path
 
 		tmpdir = "/tmp/organamer-"+time.strftime("%Y%m%d_%H%M%S")
+		os.makedirs(tmpdir)
 
 		_,_,ip,share,files_path = source_root.split("/", 4)
-		call(["smbclient", "//"+ip+"/"+share, "-c", "'lcd "+tmpdir+"; cd"+files_path+"; prompt; mget *'", "-U "+user_password])
+		lcd_part = "lcd "+tmpdir+"; cd "+files_path+"; prompt; mget *"
+		print lcd_part
+		print list2cmdline(["smbclient", "//"+ip+"/"+share, "-U", user_password, "-c", lcd_part])
+		call(["smbclient", "//"+ip+"/"+share, "-U", user_password, "-c", lcd_part])
 		source_root = tmpdir
 	#END smb capability
 
@@ -109,63 +109,83 @@ def reposit(destination_root, source_root, digits=4, letters=1, prefix=None, par
 	for root, dirs, files in os.walk(source_root):
 		for name in files:
 			source_files_list.append(os.path.join(root, name))
-
-	lastfile,lastfile_pair = pair_lastfile(destination_files_list, source_files_list)
-	digits_start = int(os.path.splitext(lastfile)[0][-digits:])
-	letters_start = os.path.splitext(lastfile)[0][-(digits+letters):-digits]
-	letters_start_index = string.lowercase.index(letters_start)
-
 	source_files_list = sorted(source_files_list)
-	old_names = source_files_list[source_files_list.index(lastfile_pair)+1:]
 
-	if parent_prefix:
-		if destination_root[-1] == "/":
-			destination_root = destination_root[:-1]
-		prefix = os.path.basename(destination_root)
+	if len(destination_files_list) == 0:
+		old_names = source_files_list
+		new_names=iterative_rename(0, old_names, destination_root, prefix="pcr_")
+	else:
+		lastfile,lastfile_pair = pair_lastfile(destination_files_list, source_files_list)
+		digits_start = int(os.path.splitext(lastfile)[0][-digits:])
+		letters_start = os.path.splitext(lastfile)[0][-(digits+letters):-digits]
+		letters_start_index = string.lowercase.index(letters_start)
 
-	new_names = []
-	digits_new = digits_start+1
-	count=0
+		old_names = source_files_list[source_files_list.index(lastfile_pair)+1:]
+		if len(old_names) == 0:
+			print("No files found to reposit. Aborting.")
+			quit()
 
-	while digits_new <= 10**digits - 1 and count <= len(old_names)-1:
-		source_file_name, extension = os.path.splitext(old_names[count])
-		#make sure files with the same path but different extensions keep the same name:
-		if source_file_name == os.path.splitext(old_names[count-1])[0]:
-			digits_new -= 1
-		if digits_new == 10**digits:
-			digits_new = 0
-			letters_start_index += 1
-			letters_start = string.lowercase[letters_start_index]
-		#create formatting template of length `digits`:
-		formatting_string = "%0"+str(digits)+"d"
-		padded_digits = formatting_string % digits_new
-		new_name = os.path.join(destination_root, letters_start, prefix+"_"+letters_start+padded_digits+extension)
-		new_names.append(new_name)
-		count += 1
-		digits_new += 1
+		if parent_prefix:
+			if destination_root[-1] == "/":
+				destination_root = destination_root[:-1]
+			prefix = os.path.basename(destination_root)
+			prefix += "_"
+
+		# don't start numbering at the last digit, otherwise you would overwrite the file
+		digits_start += 1
+		new_names = iterative_rename(digits_start, old_names, destination_root, letters_start_index, prefix=prefix)
 
 	if len(old_names) != len(new_names):
 		raise RuntimeError("Lists of old and new filenames are not of the same length. Unsafe to continue")
 
-	if len(old_names) == 0:
-		print("No files found to reposit. Aborting.")
-		quit()
+	prompt_and_copy(old_names, new_names, "Review the above operations list carefully and enter 'yes' to continue or 'no' to abort.")
+
+def iterative_rename(digits_start, old_names, destination_root="/tmp", letters_start_index=None, prefix="", digits=4):
+	count=0
+	new_names=[]
+	while digits_start <= 10**digits - 1 and count <= len(old_names)-1:
+		source_file_name, extension = os.path.splitext(old_names[count])
+		#make sure files with the same path but different extensions keep the same name:
+		if source_file_name == os.path.splitext(old_names[count-1])[0]:
+			digits_start -= 1
+		if digits_start == 10**digits:
+			digits_start = 0
+			if letters_start_index:
+				letters_start_index += 1
+				letters_start = string.lowercase[letters_start_index]
+		if not letters_start_index:
+			letters_start=""
+		#create formatting template of length `digits`:
+		formatting_string = "%0"+str(digits)+"d"
+		padded_digits = formatting_string % digits_start
+		#concatenate the name:
+		new_name = os.path.join(destination_root, letters_start, prefix+letters_start+padded_digits+extension)
+		new_names.append(new_name)
+		count += 1
+		digits_start += 1
+	return new_names
+
+def prompt_and_copy(files_from, files_to, prompt_message="Copy? [yes/no]", prompt=True):
+	"""
+	Print a prompt for the copy phase, and if answered yes, copy.
+	"""
+	from shutil import copyfile
 
 	if prompt:
-		for i in range(len(old_names)):
-			print("Preparing to copy `"+str(old_names[i])+"` to `"+str(new_names[i])+"`.")
-		if not query_yes_no("Review the above operations list carefully and enter 'yes' to continue or 'no' to abort."):
+		for i in range(len(files_from)):
+			print("Preparing to copy `"+str(files_from[i])+"` to `"+str(files_to[i])+"`.")
+		if not query_yes_no(prompt_message):
 			quit()
 
-	for i in range(len(old_names)):
-		print("Copying `"+str(old_names[i])+"` to `"+str(new_names[i])+"`.")
-		copyfile(old_names[i], new_names[i])
+	for i in range(len(files_from)):
+		print("Copying `"+str(files_from[i])+"` to `"+str(files_to[i])+"`.")
+		copyfile(files_from[i], files_to[i])
 		print("Finished!")
 
-def query_yes_no(question, default="no"):
-	"""Ask a yes/no question via raw_input() and return their answer.
+def query_yes_no(prompt_message, default="no"):
+	"""Print a yes/no prompt via raw_input() and return the answer.
 
-	"question" is a string that is presented to the user.
+	"prompt_message" is a string that is presented to the user.
 	"default" is the presumed answer if the user just hits <Enter>.
 		It must be "yes" (the default), "no" or None (meaning
 		an answer is required of the user).
@@ -188,7 +208,7 @@ def query_yes_no(question, default="no"):
 		raise ValueError("invalid default answer: '%s'" % default)
 
 	while True:
-		sys.stdout.write(question + prompt)
+		sys.stdout.write(prompt_message + prompt)
 		choice = raw_input().lower()
 		if default is not None and choice == '':
 			return valid[default]
