dcba0507fdbcdf78aae5f2068ce226d8f27ad3eb
Horea Christian <h.chr@mail.ru>
2015-05-15 09:28:35 +0200

    removed static prefix value for the reformat function

--- src/organamer/base.py
+++ src/organamer/base.py
@@ -43,7 +43,7 @@ def reformat(root_directory, digits=4, letters_start_index=None, prefix=None, pr
 			if name not in exclude:
 				original_files_list.append(os.path.join(root, name))
 	original_files_list = sorted(original_files_list)
-	new_files_list = iterative_rename(0, original_files_list,root_directory, letters_start_index=letters_start_index, prefix="pcr_", digits=digits)
+	new_files_list = iterative_rename(0, original_files_list,root_directory, letters_start_index=letters_start_index, prefix=prefix, digits=digits)
 	prompt_and_copy(original_files_list, new_files_list,
 					"\nThe original file locations above will be DELETED after copying.\nReview the above operations list carefully and enter 'yes' to continue or 'no' to abort."
 					)
