f8a59482f2302d2c2ad63c5b4681e0718354a137
Horea Christian <horea.christ@yandex.com>
2016-06-20 01:14:52 +0200

    Added new module for extracting files from "useless" directories

---
+++ src/organamer/extractor.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+__author__ = 'Horea Christian'
+import os
+import string
+import hashlib
+from shutil import copyfile
+from base import prompt_and_copy
+
+def file_extractor(base_dir):
+	"""Renames files to their parent directory name, and numerates them in case there is more than one per folder
+
+	Mandatory Argumens:
+	base_dir -- the directory in which to query for sub-directories whose files to rename
+
+	ATTENTION:
+	there are still multiple issues with this module:
+	* It is not tested on subdirectories of subdirectories
+	* It does not provide the option to filter the files to be renamed by format
+	"""
+	old_filenames = []
+	new_filenames = []
+	for root, dirs, files in os.walk(base_dir):
+		# print(1,root,dirs,files)
+		if dirs==[]:
+			for ix, old_name in enumerate(sorted(files)):
+				new_name = old_name
+				_,new_filename_extension = os.path.splitext(old_name)
+				# print(2,root, dirs)
+				# print(3,new_filename_extension)
+				old_filenames.append(os.path.join(root, old_name))
+				if len(files) >=2:
+					number="_"+str(ix)
+				else:
+					number=""
+				new_filenames.append(os.path.normpath(root)+number+new_filename_extension)
+
+	prompt_and_copy(old_filenames, new_filenames, "Review the above operations list carefully and enter 'yes' to continue or 'no' to abort.")
