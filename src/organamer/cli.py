2e1ba71a1523657c4f94be35ec92a4862924989e
Horea Christian <horea.christ@yandex.com>
2016-08-06 22:24:09 +0200

    fixed cli binding default that did not match function default

--- src/organamer/cli.py
+++ src/organamer/cli.py
@@ -8,7 +8,7 @@ def reposit():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("destination", help="Path to store files into (excluding alphanumeric storage directories)", type=str)
 	parser.add_argument("source", help="Path to reposit files from (all subdirectories will be crawled!)", type=str, nargs="*")
-	parser.add_argument("-l", "--letters", help="Prepend the specified number of letters to the incremental nummeration (default is 1).", default=1, type=int)
+	parser.add_argument("-l", "--letters", help="Prepend the specified number of letters to the incremental nummeration (default is 1).", default=0, type=int)
 	parser.add_argument("-e", "--extensions", help="Consider only files with this extension (can be used repeatedly for multiple extensions).", type=str, action="append")
 	parser.add_argument("-p", "--prefix", help="Add this prefix to all files.", default="", type=str)
 	parser.add_argument("-a", "--parent-prefix", help="Add the name of the root dir as a prefix to all files (defult FALSE).", action="store_true")
