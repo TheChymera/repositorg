17cc521de4593355857cb2314629a0279cc3bdc4
Horea Christian <horea.christ@yandex.com>
2017-10-16 01:22:46 +0200

    New audioproc function and better parameter ordering for vidproc

--- repositorg/cli.py
+++ repositorg/cli.py
@@ -8,12 +8,12 @@ def main():
 	try:
 		from base import reposit, reformat
 		from extractor import redundant
-		from processing import vidproc
+		from processing import vidproc, audioproc
 	except ImportError:
 		from .base import reposit, reformat
 		from .extractor import redundant
-		from .processing import vidproc
-	argh.dispatch_commands([reposit, reformat, redundant, vidproc])
+		from .processing import vidproc, audioproc
+	argh.dispatch_commands([reposit, reformat, redundant, vidproc, audioproc])
 
 if __name__ == '__main__':
 	main()
