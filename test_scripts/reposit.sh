5d2d2c655465e347bb3cc17ec718858bf493416e
Horea Christian <horea.christ@yandex.com>
2017-11-15 03:33:46 +0100

    Adapted to new data structure

--- test_scripts/reposit.sh
+++ test_scripts/reposit.sh
@@ -1,5 +1,5 @@
 # This runs an example repositorg command:
-repositorg reposit --no-ask --letters 1 -p nd750_ -e NEF JPG -d 4 ../../reposit_outdir/ ../example_data/
+repositorg reposit --no-ask --letters 1 -p "nd750_" -e "NEF" "JPG" -d 4 "../../reposit_outdir/" "../example_data/source/"
 
 # This checks that the output is actually correct:
 if [ ! -f "../../reposit_outdir/a/nd750_a0018.JPG" ]; then
