c7564c50b7ff7c36ff4fb414922dd8737607f4a3
Horea Christian <horea.christ@yandex.com>
2018-06-30 04:17:56 +0200

    New regex-in repositing function

--- repositorg/base.py
+++ repositorg/base.py
@@ -4,9 +4,11 @@ __author__ = 'Horea Christian'
 import argh
 import os
 import string
-import hashlib
+import re
 import warnings
 from shutil import copyfile
+
+from repositorg.utils import sha256_hashfile, CaseFormatter, query_yes_no
 
 def rename(root_dir,
 	strip_string="",
@@ -139,25 +141,226 @@ def pair_lastfile(destination_files, source_files):
 		print("No pair for the last file from "+str(destination_dir)+", namely "+str(lastfile)+" was found.")
 		return [lastfile, lastfile_pair]
 
-def sha256_hashfile(file_path, blocks="all"):
-	hasher = hashlib.sha256()
-	blocksize = hasher.block_size
-	try:
-		afile = open(file_path, "rb")
-	except IOError:
-		return
-	buf = afile.read(blocksize)
-	block_count=0
-	while len(buf) > 0:
-		if blocks == "all":
-			pass
-		elif block_count > blocks:
-			break
-		hasher.update(buf)
-		buf = afile.read(blocksize)
-		block_count += 1
+@argh.arg('-d', '--digits')
+@argh.arg('-n', '--numbering-start', type=int)
+@argh.arg('-l', '--letters-start-index', type=int)
+def reposit(in_root, out_root,
+	in_regex='.*',
+	out_regex='.*',
+	out_string='',
+	digits=4,
+	exclude=["Thumbs.db"],
+	letters=0,
+	letters_start_index=None,
+	numbering_start=None,
+	parent_prefix=False,
+	prefix="",
+	no_ask=False,
+	user_password=None,
+	):
+	"""Organamer's core repositing function
 
-	return hasher.hexdigest()
+	Arguments
+	---------
+	destination_root : string
+		Reposit the files into this directory.
+	source : list
+		Reposit the files from this directory. Alternatively can contain a list of files to reposit.
+	digits : int
+		Create new file names with this many digits.
+	exclude: list
+		Exclude these file names from the repositing process.
+	extensions: list
+		Consider only files with these extensions.
+	letters : int
+		Prepend this many letters to the digits in the new file names.
+		The files are reposited in destination_root subdirectories specific for every letter.
+	letters_start_index : int
+		Start letter incremention in file name at this letter (a=0, b=1, etc.)
+	numbering_start : int
+		Start number incremention in file name at this integer.
+	parent_prefix : bool
+		Add the name of the root dir as a prefix to all new file names.
+	prefix: string
+		Add this prefix to all new file names.
+	user_password: string
+		User and password for your remote file source (format: 'user%password')
+	no_ask: bool
+		Do not ask for confirmation - setting to True is DANGEROUS!
+
+	Notes
+	-----
+	Currently breaks for letters > 1 because the string.lowercase.index() function only takes one argument.
+	"""
+
+	out_root = os.path.abspath(os.path.expanduser(out_root))
+	out_files_list = []
+	for root, dirs, files in os.walk(out_root):
+		for name in files:
+			if name not in exclude:
+				if re.match(out_regex, name):
+					out_files_list.append(os.path.join(root, name))
+	out_files_list = sorted(out_files_list)
+
+	in_files_list = []
+	for root, dirs, files in os.walk(in_root):
+		for name in files:
+			if re.match(in_regex, name):
+				in_files_list.append(os.path.join(root, name))
+
+	in_files_list = sorted(in_files_list)
+	in_files_list = [os.path.abspath(os.path.expanduser(i)) for i in in_files_list]
+
+	if len(out_files_list) == 0:
+		old_names = in_files_list
+		if not numbering_start:
+			numbering_start= 0
+		if not letters_start_index:
+			letters_start_index = 0
+	else:
+		lastfile, lastfile_pair = pair_lastfile(destination_files_list, source_files_list)
+		if not numbering_start:
+			numbering_start = int(os.path.splitext(lastfile)[0][-digits:])
+		if letters >= 1 and not letters_start_index:
+			letters_start = os.path.splitext(lastfile)[0][-(digits+letters):-digits]
+			try:
+				letters_start_index = string.lowercase.index(letters_start)
+			except AttributeError:
+				letters_start_index = string.ascii_lowercase.index(letters_start)
+		else:
+			letters_start_index = None
+
+		if lastfile_pair:
+			old_names = source_files_list[source_files_list.index(lastfile_pair)+1:]
+		else:
+			old_names = source_files_list
+
+		if len(old_names) == 0:
+			print("No files found to reposit. Exiting.")
+			quit()
+
+		if parent_prefix:
+			if destination_root[-1] == "/":
+				destination_root = destination_root[:-1]
+			prefix = os.path.basename(destination_root)
+			prefix += "_"
+
+		# don't start numbering at the last digit, otherwise you would overwrite the file
+		numbering_start += 1
+
+	new_names = generate_names(numbering_start, old_names, out_string, in_regex, out_root, letters_start_index, digits=digits)
+
+	if len(old_names) != len(new_names):
+		raise RuntimeError("Lists of old and new filenames are not of the same length. Unsafe to continue")
+
+	prompt_and_copy(old_names, new_names,
+		prompt = not no_ask,
+		prompt_message="Review the above operations list carefully and enter 'yes' to continue or 'no' to abort.",
+		)
+
+def generate_names(digits_start, old_names, out_string, in_regex,
+	out_root="/tmp/repositorg",
+	letters_start_index=None,
+	digits=4,
+	):
+	count=0
+	new_names=[]
+	while digits_start <= 10**digits - 1 and count <= len(old_names)-1:
+		#make sure files with the same path but different extensions keep the same name:
+		#for lists of length 1, the last element is the only element, thus we require the list to be of length 2 at least
+		if os.path.splitext(old_names[count])[0] == os.path.splitext(old_names[count-1])[0] and len(old_names) >= 2:
+			digits_start -= 1
+		if digits_start == 10**digits:
+			digits_start = 0
+			if letters_start_index or letters_start_index == 0:
+				letters_start_index += 1
+		if letters_start_index or letters_start_index == 0:
+			try:
+				letters_start = string.lowercase[letters_start_index]
+			except AttributeError:
+				letters_start = string.ascii_lowercase[letters_start_index]
+		else:
+			letters_start=""
+		#create formatting template of length `digits`:
+		formatting_string = "%0"+str(digits)+"d"
+		padded_digits = formatting_string % digits_start
+		#Source variables from source file name:
+		substitutions = re.match(in_regex, os.path.basename(old_names[count]))
+		substitutions = substitutions.groupdict()
+		substitutions['LETTERS'] = letters_start
+		substitutions['DIGITS'] = padded_digits
+		#Format the name:
+		myformatter = CaseFormatter()
+		new_name = myformatter.format(out_string, **substitutions)
+		#concatenate the path:
+		new_name = os.path.join(out_root, new_name)
+		new_names.append(new_name)
+		count += 1
+		digits_start += 1
+	return new_names
+
+def iterative_rename(digits_start, old_names,
+	destination_root="/tmp",
+	letters_start_index=None,
+	prefix="",
+	digits=4,
+	):
+	count=0
+	new_names=[]
+	while digits_start <= 10**digits - 1 and count <= len(old_names)-1:
+		source_file_name, extension = os.path.splitext(old_names[count])
+		#make sure files with the same path but different extensions keep the same name:
+		#for lists of length 1, the last element is the nly element, thus we require the list to be of length 2 at least
+		if source_file_name == os.path.splitext(old_names[count-1])[0] and len(old_names) >= 2:
+			digits_start -= 1
+		if digits_start == 10**digits:
+			digits_start = 0
+			if letters_start_index or letters_start_index == 0:
+				letters_start_index += 1
+		if letters_start_index or letters_start_index == 0:
+			try:
+				letters_start = string.lowercase[letters_start_index]
+			except AttributeError:
+				letters_start = string.ascii_lowercase[letters_start_index]
+		else:
+			letters_start=""
+		#create formatting template of length `digits`:
+		formatting_string = "%0"+str(digits)+"d"
+		padded_digits = formatting_string % digits_start
+		#concatenate the name:
+		new_name = os.path.join(destination_root, letters_start, prefix+letters_start+padded_digits+extension)
+		new_names.append(new_name)
+		count += 1
+		digits_start += 1
+	return new_names
+
+def prompt_and_copy(files_from, files_to,
+	detele_source=False,
+	prompt_message="Copy?",
+	prompt=True,
+	):
+	"""
+	Print a prompt with the summary of the copy phase, and if answered yes, copy.
+	"""
+
+	if prompt:
+		for i in range(len(files_from)):
+			print("Preparing to copy '{}' to '{}'.".format(str(files_from[i]),str(files_to[i])))
+		if not query_yes_no(prompt_message):
+			quit()
+
+	for i in range(len(files_from)):
+		print("Copying '{}' to '{}'.".format(str(files_from[i]),str(files_to[i])))
+		if not os.path.exists(os.path.dirname(files_to[i])):
+			os.makedirs(os.path.dirname(files_to[i]))
+		copyfile(files_from[i], files_to[i])
+		print("Finished!")
+		if detele_source:
+			print("Deleting '{}'".format(str(files_from[i])))
+			os.remove(files_from[i])
+			print("Deleted!")
+
+
 
 @argh.arg('-d', '--digits')
 @argh.arg('-e', '--extensions', nargs='+', type=str)
@@ -165,7 +368,7 @@ def sha256_hashfile(file_path, blocks="all"):
 @argh.arg('-l', '--letters-start-index', type=int)
 @argh.arg('-p', '--prefix')
 @argh.arg('source', nargs='+', type=str)
-def reposit(destination_root, source,
+def reposit_legacy(destination_root, source,
 	digits=4,
 	exclude=["Thumbs.db"],
 	extensions=[],
@@ -313,104 +516,3 @@ def reposit(destination_root, source,
 		prompt_message="Review the above operations list carefully and enter 'yes' to continue or 'no' to abort.",
 		)
 
-def iterative_rename(digits_start, old_names,
-	destination_root="/tmp",
-	letters_start_index=None,
-	prefix="",
-	digits=4,
-	):
-	count=0
-	new_names=[]
-	while digits_start <= 10**digits - 1 and count <= len(old_names)-1:
-		source_file_name, extension = os.path.splitext(old_names[count])
-		#make sure files with the same path but different extensions keep the same name:
-		#for lists of length 1, the last element is the nly element, thus we require the list to be of length 2 at least
-		if source_file_name == os.path.splitext(old_names[count-1])[0] and len(old_names) >= 2:
-			digits_start -= 1
-		if digits_start == 10**digits:
-			digits_start = 0
-			if letters_start_index or letters_start_index == 0:
-				letters_start_index += 1
-		if letters_start_index or letters_start_index == 0:
-			try:
-				letters_start = string.lowercase[letters_start_index]
-			except AttributeError:
-				letters_start = string.ascii_lowercase[letters_start_index]
-		else:
-			letters_start=""
-		#create formatting template of length `digits`:
-		formatting_string = "%0"+str(digits)+"d"
-		padded_digits = formatting_string % digits_start
-		#concatenate the name:
-		new_name = os.path.join(destination_root, letters_start, prefix+letters_start+padded_digits+extension)
-		new_names.append(new_name)
-		count += 1
-		digits_start += 1
-	return new_names
-
-def prompt_and_copy(files_from, files_to,
-	detele_source=False,
-	prompt_message="Copy?",
-	prompt=True,
-	):
-	"""
-	Print a prompt with the summary of the copy phase, and if answered yes, copy.
-	"""
-
-	if prompt:
-		for i in range(len(files_from)):
-			print("Preparing to copy '{}' to '{}'.".format(str(files_from[i]),str(files_to[i])))
-		if not query_yes_no(prompt_message):
-			quit()
-
-	for i in range(len(files_from)):
-		print("Copying '{}' to '{}'.".format(str(files_from[i]),str(files_to[i])))
-		if not os.path.exists(os.path.dirname(files_to[i])):
-			os.makedirs(os.path.dirname(files_to[i]))
-		copyfile(files_from[i], files_to[i])
-		print("Finished!")
-		if detele_source:
-			print("Deleting '{}'".format(str(files_from[i])))
-			os.remove(files_from[i])
-			print("Deleted!")
-
-
-
-def query_yes_no(prompt_message, default_choice="no"):
-	"""Print a yes/no prompt via raw_input() and return the answer.
-
-	"prompt_message" is a string that is presented to the user.
-	"default_choice" is the presumed answer if the user just hits <Enter>.
-		It must be "yes" (the default_choice), "no" or None (meaning
-		an answer is required of the user).
-
-	The "answer" return value is one of "yes" or "no".
-
-	Author: fmark (http://stackoverflow.com/users/103225/fmark)
-	"""
-	import sys
-
-	valid = {"yes": True, "y": True, "ye": True,
-			 "no": False, "n": False}
-	if default_choice is None:
-		prompt = " [y/n] "
-	elif default_choice == "yes":
-		prompt = " [Y/n] "
-	elif default_choice == "no":
-		prompt = " [y/N] "
-	else:
-		raise ValueError("invalid default answer: '%s'" % default_choice)
-
-	while True:
-		sys.stdout.write(prompt_message + prompt)
-		try:
-			choice = raw_input().lower()
-		except NameError:
-			choice = input().lower()
-		if default_choice is not None and choice == '':
-			return valid[default_choice]
-		elif choice in valid:
-			return valid[choice]
-		else:
-			sys.stdout.write("Please respond with 'yes' or 'no' "
-							 "(or 'y' or 'n').\n")
