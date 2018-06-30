740a84beb4a60becf1b614322a4b4d3e2f52ec3c
Horea Christian <horea.christ@yandex.com>
2018-06-30 04:22:14 +0200

    Updated tests to reflect new repositing functio to reflect new

--- test_scripts/reposit.sh
+++ test_scripts/reposit.sh
@@ -1,16 +1,15 @@
 # This runs an example repositorg command:
-repositorg reposit --no-ask --letters 1 -p "nd750_" -e "NEF" "JPG" -d 4 "../../reposit_outdir/" "../example_data/source/"
+repositorg reposit --no-ask --in-regex '^(?P<subject>.+?)_(?P<slice>.+?)_(?P<zoom>[0-9]+?)x_w[1-9]*(?P<modality>.+?)( .*)?\.(?P<extension>.+?)$' --out-string 'sub-{subject}/sub-{subject}_slice-{slice!l}_zoom-{zoom}_{modality!l}.{extension!l}' ../example_data/source_b/ ../../reposit_outdir_b/
 
 # This checks that the output is actually correct:
-if [ ! -f "../../reposit_outdir/a/nd750_a0018.JPG" ]; then
+if [ ! -f "../../reposit_outdir_b/a/sub-5700/sub-5700_slice-a4_zoom-5_egfp.tif" ]; then
     exit 1
 fi
 
-if [ ! -f "../../reposit_outdir/a/nd750_a0018.NEF" ]; then
-    echo "Lalal"
+if [ ! -f "../../reposit_outdir_b/a/sub-5700/sub-5700_slice-a4_zoom-5_dsred.tif" ]; then
     exit 1
 fi
 
-if [ -f "../../reposit_outdir/a/nd750_a0019.JPG" ]; then
+if [ -f "../../reposit_outdir_b/a/sub-5700/sub-5700_slice-a4_zoom-5_transmission.tif" ]; then
 	exit 1
 fi
