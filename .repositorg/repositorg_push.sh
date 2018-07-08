253d2cdba5541829e7b3a85e9ce8bda955220a65
Horea Christian <horea.christ@yandex.com>
2018-07-08 21:51:09 +0200

    Added more examples

---
+++ .repositorg/repositorg_push.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+# This script should be kept and run on the data acquisition machine.
+# Given to the nomenclature below: example-source and NOT analysishost!
+rsync -avP /incoming/data anaylsishost:/var/tmp/repositorg/example-source
+ssh analysishost ~/.repositorg/sources/example-source_post-hook.sh
--- .repositorg/sources/example-device_post-hook.sh
+++ .repositorg/sources/example-device_post-hook.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 
-# Specify the path of the data relative to the device root.
-# This is left empty if the data is directly under the top level root of the device.
-REL_PATH="transfer0/MyLab/"
+# Specify the path of the data.
+SOURCE_ID="transfer0/Chrisrtian/"
 
 # Concatenate workflow here:
-repositorg fetch --in-path "${REL_PATH}" "${1}" "${2}"
 repositorg imgproc --unstack\
 	--parameters "-auto-level -gravity Center -extent 2000x2000"\
-	"/var/tmp/repositorg/${2}/"*TIF
+	"/var/tmp/repositorg/${SOURCE_ID}/"*TIF
 repositorg reposit --no-ask\
 	--in-regex '^(?P<subject>.+?)_(?P<slice>.+?)_(?P<zoom>[0-9]+?)x_w[1-9]'\
 '*(?P<modality>.+?)( .*)?_scene(?P<scene>.+?)\.(?P<extension>.+?)$'\
 	--out-string 'sub-{subject}/sub-{subject}_slice-{slice!l}_zoom-{zoom}_'\
 'scene-{scene}_{modality!l}.{extension!l}'\
-	"/var/tmp/repositorg/${2}/"\
+	"/var/tmp/repositorg/${SOURCE_ID}/"\
 	"~/histology/"
 
 # You should really think about cleaning up, though ideally only after having tested the workflow.
-rm -rf "/var/tmp/repositorg/${2}"
+rm -rf "/var/tmp/repositorg/${SOURCE_ID}"
